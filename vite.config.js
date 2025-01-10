32426d6387a7b7683f90200ad395984eea70dce8
maksimilz <maksimilz@mail.ru>
2025-01-10 21:06:12 +0300

    Fix router base3

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/diplom/', // Укажите имя репозитория вместо "diplom"
+  base: '/diplom/',
   plugins: [
     vue(),
     vueDevTools(),
